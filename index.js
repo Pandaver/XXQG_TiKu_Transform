5fd36ed093425c54b5ec42274c368f1783bf1af6
Pandaver <62326552+Pandaver@users.noreply.github.com>
2022-05-27 16:32:17 +0800

    Add SQLite transform

--- index.js
+++ index.js
@@ -2,25 +2,38 @@ const https = require('https')
 const utils = require('./utils')
 const config = require('./config')
 
+
 https.get( `${config.githubRawProxy}mondayfirst/XXQG_TiKu/main/题库_排序版.json`, async res => {
   res.setEncoding('utf-8')
-  let mf_tiku = ''
+  utils.saveToFile('', './XXQG_TiKu.json')
+  console.info('mondayfirst/XXQG_TiKu Downloading...')
   res.on('data', append => {
-    console.info('mondayfirst/XXQG_TiKu Downloading...')
-    mf_tiku += append
+    utils.addToFile(append, './XXQG_TiKu.json')
   }).on('end', () => {
-    console.info('mondayfirst/XXQG_TiKu Download Complete.')
-    // console.info('mondayfirst/XXQG_TiKu Save To File...')
-    // utils.toFile(mf_tiku, './XXQG_TiKu.json')
-    // console.info('Complete.')
-    console.info('mondayfirst/XXQG_TiKu Transform To TiKu Object...')
-    const obj = utils.mfToObj(mf_tiku)
     console.info('Complete.')
-    console.info('TiKu Object Transform To Twelve-blog/Study_hamibot...')
-    const tw_tiku = utils.objToTw(obj)
+    console.info('Reading XXQG_TiKu.json...')
+    const mf_tiku = utils.readFileToJson('./XXQG_TiKu.json')
     console.info('Complete.')
-    console.info('mondayfirst/XXQG_TiKu Save To File...')
-    utils.toFile(tw_tiku, './question')
-    console.info('Complete.')
+    console.info('Transform...')
+    transform(mf_tiku)
   })
 })
+
+async function transform(mf_obj) {
+  utils.saveToFile('', './question')
+  // utils.saveToFile('', './QuestionBank.sql')
+  const db = await utils.initDb('./QuestionBank.db')
+  let i = 0
+  for (let q in mf_obj) {
+    const obj = utils.mfToObj(q, mf_obj[q])
+    const tw_json = utils.objToTwJson(obj)
+    const tw_sql = utils.objToTwSQL(obj)
+    await db.prepare(tw_sql).run()
+    i++
+    utils.addToFile(tw_json + '\n', './question')
+    // utils.addToFile(tw_sql + '\n', './QuestionBank.sql')
+  }
+  db.close()
+  console.info(`Processed ${i} data.`)
+  console.info('Complete.')
+}
--- utils.js
+++ utils.js
@@ -1,81 +1,112 @@
 const fs = require('fs')
+const Database = require('better-sqlite3')
 
-/**
- * mondayfirst/XXQG_TiKu格式题库转对象
- * @param {string} mf_json_string
- * @return {array}
- * Return Example
- * [{
- *   title: '根据《中华人民共和国土壤污染防治法》，未达到土壤污染风险评估报告确定的风险管控、修复目标的建设用地地块，禁止开工建设任何与        无关的项目。',
- *   options: [
- *     { value: 'A', text: '后期管理' },
- *     { value: 'B', text: '效果评估' },
- *     { value: 'C', text: '调查评估' },
- *     { value: 'D', text: '风险管控、修复' }
- *   ],
- *   answer: [ { value: 'D', text: '风险管控、修复' } ]
- * }]
- */
-exports.mfToObj = mf_json_string => {
-  const mf_obj = JSON.parse(mf_json_string)
-  const questions = []
-  for (let q in mf_obj) {
-    let a = mf_obj[q]
-    const answer = []
-    q = q.split('|')
-    const options = []
-    let key = ['title', 'A', 'B', 'C', 'D', 'E', 'F', 'G']
-    for (let i = 1; i < q.length; i++) {
-      const option = {
-        value: key[i],
-        text: q[i]
-      }
-      options.push(option)
-      if (q[i] === a) {
-        answer.push(option)
-      }
-    }
-    questions.push({
-      title: q[0],
-      options,
-      answer
-    })
-  }
-  return questions
+
+exports.readFileToJson = path => {
+  let data = fs.readFileSync(path, 'utf-8')
+  return JSON.parse(data)
 }
 
 /**
- * 题库对象转Twelve-blog/Study_hamibot格式题库
- * @param obj
- * @return {string}
- */
-exports.objToTw = obj => {
-  let questions = ''
-  for (let i = 0; i < obj.length; i++) {
-    const q = obj[i]
-    questions += `${q.answer[0].value} ${q.title}`
-    for (let j = 0; j < q.options.length; j++) {
-      const option = q.options[j]
-      questions += ' ' + option.text
-    }
-    questions += '\n'
-  }
-  questions = questions.substring(0, questions.length - 1)
-  return questions
-}
-
-/**
- * 保存文件
+ * 写入文件
  * @param content
  * @param path
  */
-exports.toFile = (content, path) => {
-  fs.writeFile(path, content, 'utf-8', err => {
-    if (err) {
-      console.log('Write File Failed!')
-      throw err
-    }
-  })
+exports.saveToFile = (content, path) => {
+  return fs.writeFileSync(path, content, 'utf-8')
 }
 
+/**
+ * 追加内容
+ * @param content
+ * @param path
+ */
+exports.addToFile = (content, path) => {
+  return fs.appendFileSync(path, content, 'utf-8')
+}
 
+/**
+ * mondayfirst/XXQG_TiKu格式题库转对象
+ * @param question
+ * @param answer
+ * @returns {{options: *[], answers: *[], title: *}}
+ */
+exports.mfToObj = (question, answer) => {
+  const a = []
+  const q = question.split('|')
+  const options = []
+  let key = ['title', 'A', 'B', 'C', 'D', 'E', 'F', 'G']
+  for (let i = 1; i < q.length; i++) {
+    const option = {
+      value: key[i],
+      text: q[i]
+    }
+    options.push(option)
+    if (q[i] === answer) {
+      a.push(option)
+    }
+  }
+  return {
+    title: q[0],
+    options,
+    answers: a
+  }
+}
+
+/**
+ * 题库对象转Twelve-blog/Study_hamibot Json格式题库
+ */
+exports.objToTwJson = obj => {
+  let q = ''
+  for (let i = 0; i < obj.answers.length; i++) {
+    q += `${obj.answers[i].value}`
+  }
+  q += ` ${obj.title}`
+  for (let j = 0; j < obj.options.length; j++) {
+    const option = obj.options[j]
+    q += ' ' + option.text
+  }
+  return q
+}
+
+/**
+ * 题库对象转Twelve-blog/Study_hamibot SQL格式题库
+ */
+exports.objToTwSQL = obj => {
+  let sql = ''
+  sql += `INSERT INTO 'tiku' VALUES ('${obj.title}`
+  for (let i = 0; i < obj.options.length; i++) {
+    sql += `|${obj.options[i].text}`
+  }
+  sql += `','${obj.answers[0].text}', NULL, '${obj.answers[0].value}', '${time()}');`
+  return sql
+}
+
+/**
+ * 格式化输出时间YYYY mm-DD hh:mm:ss
+ * @param d
+ * @param dash
+ * @param colon
+ * @returns {string}
+ */
+function time(d=new Date(), dash='-',colon=':') {
+  function pad(n) {
+    return n < 10 ? '0' + n : n
+  }
+  return d.getFullYear() + dash +
+    pad(d.getMonth() + 1) + dash +
+    pad(d.getDate()) + ' ' +
+    pad(d.getHours()) + colon +
+    pad(d.getMinutes()) + colon +
+    pad(d.getSeconds())
+}
+
+exports.initDb = async path => {
+  if (fs.existsSync(path)) {
+    console.info(`Delete old ${path} file.`)
+    fs.unlinkSync(path)
+  }
+  const db = new Database(path)
+  await db.exec(`CREATE TABLE 'tiku' ('question' CHAR(231), 'answer' CHAR(29), 'wrongAnswer' CHAR(23), 'option' CHAR(10), 'datetime' int)`)
+  return db
+}
